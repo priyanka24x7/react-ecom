78ba2f5a97ff53de2fb94c481dcf58940c5b3ce2
Priyanka Pandey <priyawebweb@gmail.com>
2021-06-07 13:47:08 -0700

    categories ui changes

--- buy-more/src/components/products/Product-list.jsx
+++ buy-more/src/components/products/Product-list.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import '../products/Products.scss';
 import {ProductsContext} from '../../Global/ProductsContext';
+import ProductsCat from './ProductCategories';
 
 function ProductList() {
   const {list} = useContext(ProductsContext);
@@ -8,13 +9,15 @@ function ProductList() {
   return (
     <div className="list-wrapper">
         <h3>Products</h3>
+        <div className="main-container d-flex">
+            <ProductsCat />
         <div className="p-wrapper">
         {
           list.map((product) => (
             <div className="products">
               <div key={product.categoryId}></div>
               <div className="g-img">
-                <img src={product.productImage} alt="buy more bhendi"/>
+                <img src={product.productImage} alt="dals"/>
               </div>
               <div className="name">{product.productName}<span>({product.weight})</span></div>
               <div className="price">₹{product.unitPrice}</div>
@@ -25,6 +28,7 @@ function ProductList() {
             </div>
           ))
         }
+        </div>
         </div>
     </div>
   );
---
+++ buy-more/src/components/products/ProductCategories.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const ProductsCat = () => {
+    return(
+        <>
+            <div className="category-section">
+                <ul>
+                    <li>All Products</li>
+                    <li>Dals</li>
+                    <li>Pulses</li>
+                    <li>Dry Fruits</li>
+                    <li>Cooking Oil</li>
+                    <li>Ghee &amp; Vanaspati</li>
+                    <li>Flours &amp; Grains</li>
+                    <li>Rice &amp; Rice Products</li>
+                    <li>Masala &amp; Spices</li>
+                    <li>Salt/Sugar/Jaggery</li>
+                </ul>
+            </div>
+        </>
+    ) 
+}
+export default ProductsCat;
